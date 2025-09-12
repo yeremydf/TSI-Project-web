cc15bcf7c09c2a5b0a2f83e7f44d0fa520b7c5d8
Tryingt0Dev <102566524+Tryingt0Dev@users.noreply.github.com>
2025-09-12 20:56:10 -0300

    up

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Libro extends Model
 {
-    //
+    protected $primaryKey = 'id_libro_interno';
+
+    protected $fillable = [
+        'isbn_libro',
+        'nom_libro',
+        'fecha_publicacion',
+        'editorial',
+        'genero_literario',
+        'cantidad_disponible',
+        'id_ubicacion',
+        'id_detalle_autores',
+        'imagen',
+    ];
 }
